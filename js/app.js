9be17841d114999f53fc8cf8d55dbc11df2ea00d
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-04-07 00:40:03 -0300

    se agregó la logica de ocultar las imagenes de elección por el jugar y mostrar lo que elige la computadora, tambien se deshabilitó el botón volver a jugar

--- js/app.js
+++ js/app.js
@@ -2,23 +2,61 @@
 Ejercicio opcional Piedra-papel-tijera 👊🏻 ✋🏻 ✌🏻 : crear un script con toda la logica necesaria que solicite al usuario un movimiento (piedra - papel - tijera) la PC elija aleatoriamente un movimiento y al final mostrar por pantalla quien gano, luego preguntar si queremos jugar otra vez.
 */
 let movimientoJugador = '';
+let botonJugardenuevo = document.getElementById('jugarNuevamente');
+let contenedorImagenesJugador = document.getElementById(
+  'contenedorImagenesJugador'
+);
+botonJugardenuevo.addEventListener('click', mostrarDivEleccionJugador);
 
 function eleccionJugador(opcion) {
   movimientoJugador = parseInt(opcion);
 
-  let contenedorImagenesJugador = document.getElementById(
-    'contenedorImagenesJugador'
-  );
-  console.log(contenedorImagenesJugador.children);
-  contenedorImagenesJugador.remove(contenedorImagenesJugador);
+  contenedorImagenesJugador.classList.add('d-none');
+  //Crear la imagen que eligió el Jugador
+  const imagen = document.createElement('img');
+  imagen.src = asignarSrcImagenJugador(opcion);
+  imagen.width = 100;
+  //imagen.classList.add('img-fluid');
+  imagen.alt = 'Imagen elegida por el Jugador';
 
-  eleccionComputadorayCompararar();
+  //Crear el div de la imagen
+  const divImagen = document.createElement('div');
+  divImagen.classList.add('divImagen');
 
-  //const jugarNuevamente = document.getElementById('jugarNuevamente');
-  //console.log(jugarNuevamente);
+  //Asignar la imagen
+  divImagen.appendChild(imagen);
+
+  const contenedor = document.getElementById('seccionPadreJugador');
+  contenedor.appendChild(divImagen, contenedor.children[2]);
+
+  eleccionComputadorayComparar();
+
+  //Activar el boton "Volver a Jugar"
+  botonJugardenuevo.classList.remove('disabled');
 }
 
-function eleccionComputadorayCompararar() {
+asignarSrcImagenJugador = (opcion) => {
+  switch (opcion) {
+    //Jugador eligio piedra
+    case 1:
+      return '/assets/img/piedra-removebg.png';
+    //Jugador eligio papel
+    case 2:
+      return '/assets/img/papel-removebg.png';
+    //Jugador eligio tijera
+    case 3:
+      return '/assets/img/tijera-removebg.png';
+    case null:
+      alert(
+        'No ingresó ningun valor. Ingrese 1 para piedra, 2 para papel y 3 para tijera'
+      );
+      break;
+    default:
+      console.log('La opción ingresada no es válida');
+  }
+};
+
+function eleccionComputadorayComparar() {
   const movimientoComputadora = Math.floor(Math.random() * 3 + 1);
   // movimientoComputadora = 1 entonces es piedra.
   // movimientoComputadora = 2 entonces es papel.
@@ -99,5 +137,18 @@ function eleccionComputadorayCompararar() {
       break;
     default:
       console.log('La opción ingresada no es válida');
+  }
+}
+
+function mostrarDivEleccionJugador() {
+  if (contenedorImagenesJugador.classList.contains('d-none')) {
+    contenedorImagenesJugador.classList.remove('d-none');
+    const contenedor = document.getElementById('seccionPadreJugador');
+    contenedor.removeChild(contenedor.children[2]);
+    botonJugardenuevo.classList.add('disabled');
+
+    //asignar la imagen de signo de pregunta
+    const imagenComputadora = document.getElementById('imagenComputadora');
+    imagenComputadora.src = '/assets/img/signoPregunta.png';
   }
 }
